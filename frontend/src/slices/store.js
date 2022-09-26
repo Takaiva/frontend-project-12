43c2df92fc82d89e40f62c54c459587d9ae4441e
Александр Сдобников <sdobnikove@mail.ru>
2022-09-27 02:13:46 +0300

    wrap store in a function for not create instance of a store right after import as a module

--- frontend/src/slices/store.js
+++ frontend/src/slices/store.js
@@ -3,7 +3,7 @@ import channelsReducer from './channelsSlice.js';
 import messagesReducer from './messagesSlice.js';
 import modalReducer from './modalSlice.js';
 
-export default configureStore({
+export default () => configureStore({
   reducer: {
     channels: channelsReducer,
     messages: messagesReducer,

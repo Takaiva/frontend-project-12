431c4c42a5c66208363edba238df25dbd12564c3
Александр Сдобников <sdobnikove@mail.ru>
2022-09-27 02:11:47 +0300

    move socket subscription directly into init function

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
-import { Provider as StoreProvider, useDispatch } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 import filter from 'leo-profanity';
@@ -10,34 +10,29 @@ import App from './components/App.jsx';
 
 import { actions as chatActions } from './slices/messagesSlice';
 import { actions as channelsActions } from './slices/channelsSlice';
-import store from './slices/store.js';
+import getStore from './slices/store.js';
 import resources from './locales/index.js';
 
-const SocketSubscribe = ({ socket, children }) => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    socket.on('newMessage', (message) => {
-      dispatch(chatActions.addMessage(message));
-    });
-    socket.on('newChannel', (channel) => {
-      dispatch(channelsActions.addChannel(channel));
-    });
-    socket.on('renameChannel', (channel) => {
-      dispatch(channelsActions.renameChannel({
-        id: channel.id,
-        changes: { name: channel.name },
-      }));
-    });
-    socket.on('removeChannel', ({ id }) => {
-      dispatch(channelsActions.removeChannel(id));
-    });
-  }, [socket, dispatch]);
-
-  return children;
-};
-
 export default async (socket) => {
+  const store = getStore();
+  const { dispatch } = store;
+
+  socket.on('newMessage', (message) => {
+    dispatch(chatActions.addMessage(message));
+  });
+  socket.on('newChannel', (channel) => {
+    dispatch(channelsActions.addChannel(channel));
+  });
+  socket.on('renameChannel', (channel) => {
+    dispatch(channelsActions.renameChannel({
+      id: channel.id,
+      changes: { name: channel.name },
+    }));
+  });
+  socket.on('removeChannel', ({ id }) => {
+    dispatch(channelsActions.removeChannel(id));
+  });
+
   const rollbarConfig = {
     accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
     captureUncaught: true,
@@ -64,9 +59,7 @@ export default async (socket) => {
       <ErrorBoundary>
         <I18nextProvider i18n={i18n}>
           <StoreProvider store={store}>
-            <SocketSubscribe socket={socket}>
-              <App socket={socket} />
-            </SocketSubscribe>
+            <App socket={socket} />
           </StoreProvider>
         </I18nextProvider>
       </ErrorBoundary>
